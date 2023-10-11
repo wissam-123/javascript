b2873981aca5f2398a353cda1746be771d0a2f71
wissam-123 <144032700+wissam-123@users.noreply.github.com>
2023-10-11 13:02:05 +0200

    Add files via upload

---
+++ javascript6.1(maak een functie)/code.js
@@ -0,0 +1,6 @@
+function kleurEnTekstAanpassen(){
+    document.getElementById("kop1").style.color = "red";
+    document.getElementById("kop1").innerHTML = "I have changed!";
+}
+
+kleurEnTekstAanpassen();
