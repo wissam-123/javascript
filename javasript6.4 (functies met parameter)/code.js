d00390604863c1346d4815c6552e59c495910702
wissam-123 <144032700+wissam-123@users.noreply.github.com>
2023-10-11 13:06:38 +0200

    Add files via upload

---
+++ javasript6.4 (functies met parameter)/code.js
@@ -0,0 +1,23 @@
+     /* 6.4 ofening1*/
+function calculate(getal1,getal2)
+{
+ antwoord=getal1 + getal2;
+ console.log(" het antwoord van de som is: "); 
+ console.log(antwoord); 
+}
+ 
+      /*6.4 ofening2*/
+function keer5(getal1)
+{
+ antwoord=getal1 * 5;
+ console.log(" het antwoord van de som is: "); 
+ console.log(antwoord); 
+}
+ 
+       /*6.4 ofening3*/
+function calculateMinutes(seconden)
+{
+    antwoord= seconden / 60;
+    console.log(" het antwoord van de som is:"); 
+    console.log(antwoord);
+}
