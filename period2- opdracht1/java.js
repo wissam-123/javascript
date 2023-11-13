9b91687b24efe203c110be9cef00328ff807dbc2
wissam-123 <144032700+wissam-123@users.noreply.github.com>
2023-11-13 23:50:45 +0100

    Add files via upload

---
+++ period2- opdracht1/java.js
@@ -0,0 +1,10 @@
+// JavaScript om de modal en leeftijdsverificatie te beheren
+let modal = document.getElementById('cookiewall')
+
+function schowModal() {
+    modal.style.display="block";
+}
+function accepteer(){
+    modal.style.display="none";
+}
+ schowModal()
