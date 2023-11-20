5990d7956db77ae4735b8e0d29a87ba2deebee50
wissam-123 <144032700+wissam-123@users.noreply.github.com>
2023-11-20 14:54:39 +0100

    Add files via upload

---
+++ Periode 2 - Week 1 - Opdracht 3/app.js
@@ -0,0 +1,9 @@
+function submitForm() {
+var productWaarde= document.getElementById("productName").value;
+ 
+// Console.log de waarde van de ingevoerde gebruiker
+console.log("Ingevoerde productnaam: ", productWaarde);
+ 
+// Hier kun je verdere verwerking van de ingevoerde waarde uitvoeren
+// Bijvoorbeeld: het versturen van de ingevoerde waarde naar een server, verwerking van gegevens, etc.
+}
